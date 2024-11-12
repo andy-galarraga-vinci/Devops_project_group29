1913ad013b6397419a614e0006885b62f48ad310
galarraga <andy.galarraga@student.vinci.be>
2024-11-12 13:04:51 +0100

    refactor: extract the main class instanciation

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,17 +1,14 @@
 import { Main } from "../src/app.js";
 
 describe("turn on a yellow lamp per minute ", function(){
+    let main = new Main();
 
     it("Main should return Yellow (Y) when given 1 minute", function(){
-        let main = new Main();
-        
         let result = main.lastRowMinute("00:01:00");
         
         expect(result).toBe("Y") 
     })
     it("Main should return Yellow (YY) when given 2 minutes", function(){
-        let main = new Main();
-        
         let result = main.lastRowMinute("00:02:00");
         
         expect(result).toBe("YY") 
