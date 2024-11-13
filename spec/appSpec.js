0af3da12ade67afd56fabe06a6fb7410d98aa913
loumari <mehdi.loumari@student.vinci.be>
2024-11-13 14:21:43 +0100

    refactor: secondhRowHour and secondTestCases and return RRRR when given 20 hours

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -77,7 +77,8 @@ describe("turn on a red light per 5 hours", function(){
         {time: "00:00:00", expected: 'XXXX'},
         {time: "05:00:00", expected: 'RXXX'},
         {time: "10:00:00", expected: 'RRXX'},
-        {time: "15:00:00", expected: 'RRRX'}
+        {time: "15:00:00", expected: 'RRRX'},
+        {time: "20:00:00", expected: 'RRRR'}
     ];
 
     secondRowTestCases.forEach(({time, expected}) => {
