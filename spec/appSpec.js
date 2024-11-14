d6d6122435b2d8c58550ff4c09a147e9f4b320d9
galarraga <andy.galarraga@student.vinci.be>
2024-11-14 13:16:12 +0100

    test berlinClock

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -148,8 +148,20 @@ describe("Group all the previous methods to give the desired time", function(){
         const result = main.berlinClock("05:05:05");
         
         expect(result).toBe("X\nRXXX\nXXXX\nYXXXXXXXXXX\nXXXX");
-    })
+    });
+
+    it(" main should return (Y\nRRXX\nRRRX\nYYXXXXXXXXX\nYYYX) when given (06:13:06)",function(){
+
+        const result = main.berlinClock("13:13:06");
+        
+        expect(result).toBe("Y\nRRXX\nRRRX\nYYXXXXXXXXX\nYYYX");
+        console.log(result);
+
+    });
+
+    
 })
+
 
     
 
